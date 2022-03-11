cea3585e36ee2203777be3ce3b6cd1682c907cdd
Русанова Анна <rusanova.annette@gmail.com>
2022-03-11 12:27:27 +0200

    Second

--- homework.py
+++ homework.py
@@ -1,12 +1,15 @@
-from http import HTTPStatus
 import logging
 import os
 import requests
 import time
 import telegram
 import telegram.ext
-
+from http import HTTPStatus
 from dotenv import load_dotenv
+try:
+    from simplejson.errors import JSONDecodeError
+except ImportError:
+    from json.decoder import JSONDecodeError
 
 load_dotenv()
 
@@ -26,45 +29,61 @@ HOMEWORK_STATUSES = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+class Error(Exception):
+    pass
+
+
+class ErrorNotCorrectStatusCode(Error):
+    pass
+
 
 def send_message(bot, message):
     """отправляет сообщение в Telegram чат."""
+    logger = logging.getLogger(__name__)
     try:
         bot.send_message(
             chat_id=TELEGRAM_CHAT_ID,
             text=message
         )
-        logging.info(
+        logger.info(
             f'Сообщение отправленно: {message}'
         )
     except Exception as error:
         logging.error(
             f'Сообщение не отправленно: {error}'
         )
+        raise telegram.TelegramError(f'Ошибка {error}')
 
 
 def get_api_answer(current_timestamp):
     """делает запрос к единственному эндпоинту API-сервиса."""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-    response = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    try:
+        response = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    except Exception as error:
+        logging.error(f'Ошибка при запросе: {error}')
     if response.status_code != HTTPStatus.OK:
         status = response.status_code
         logging.error(f'Ошибка {status}')
-        raise Exception(f'Ошибка {status}')
+        raise ErrorNotCorrectStatusCode(f'Ошибка {status}')
     else:
-        return response.json()
+        try:
+            res = response.json()
+        except JSONDecodeError:
+            logging.error("Ошибка преобразования в JSON")
+        return res
 
 
 def check_response(response):
     """проверяет ответ API на корректность."""
     if type(response) is not dict:
         raise TypeError('Ответ API не словарь')
+    if 'homeworks' not in response:
+        logging.error('Отсутствует ключ homeworks')
+        raise KeyError('Отсутствует ключ homeworks')
     if type(response['homeworks']) is not list:
         raise TypeError('Домашнее задание прниходит не в виде списка')
-    if ('homeworks') not in response:
-        logging.error('Отсутствует ключ homeworks')
-        raise TypeError('Отсутствует ключ homeworks')
     if response.get('homeworks') is None:
         raise TypeError('Список homrworks пустой')
     return response.get('homeworks')
@@ -72,21 +91,18 @@ def check_response(response):
 
 def parse_status(homework):
     """извлекает из информации о домашней работе статус этой работы."""
-    homework_name = homework.get('homework_name')
     if 'homework_name' not in homework:
         raise KeyError(
             'В ответе не содержится ключ homework_name'
         )
-    homework_status = homework.get('status')
+    homework_name = homework.get('homework_name')
     if 'status' not in homework:
         raise KeyError(
             'В ответе не содержится ключ status'
         )
-    if homework_status not in HOMEWORK_STATUSES.keys():
-        raise ValueError(
-            f'Статус домашней работы {homework_status} некорректен'
-        )
-    verdict = HOMEWORK_STATUSES[homework_status]
+    homework_status = homework.get('status')
+    if homework_status in HOMEWORK_STATUSES.keys():
+        verdict = HOMEWORK_STATUSES[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
